416c497706bbea96dd22e0afa9322d982d662030
Amine <mohamedamin.benmakhlouf@gmail.com>
2023-07-10 18:06:34 +0200

    for_loop_basic1_assignment

---
+++ 01-python-fundamentals/for_loop_basic1.py
@@ -0,0 +1,38 @@
+for i in range(0,151):
+    print(i)
+
+print("---------------------------")
+for i in range(5,1001):
+    if i%5==0:
+        print(i)
+
+print("---------------------------")
+
+for i in range(1,101):
+    if i%5==0:
+        print(i,"coding")
+    if i%10==0:
+        print(i,"coding dojo")
+    else: print(i)
+
+print("---------------------------")
+sum=0
+for i in range(0,500001):
+    if i%2!=0:
+        sum+=i
+print(sum)
+
+print("---------------------------")
+
+for i in range(2018,-1,-4):
+    print(i)
+
+print("---------------------------")
+
+def Flex(lowNum,highNum,multi):
+    for i in range(lowNum,highNum+1):
+        if i%multi==0:
+            print(i)
+    return None
+
+Flex(2,9,3)
