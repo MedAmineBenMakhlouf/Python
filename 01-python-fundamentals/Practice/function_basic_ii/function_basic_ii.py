a6ed2ca9ac30104aaf5d89cb314f88c58656a920
Amine <mohamedamin.benmakhlouf@gmail.com>
2023-07-12 23:14:59 +0200

    function_basic_ii

---
+++ 01-python-fundamentals/Practice/function_basic_ii/function_basic_ii.py
@@ -0,0 +1,54 @@
+# def Countdown(number):
+#     if(number<0):
+#         for i in range(number,1):
+#             print(i)
+
+#         return None
+#     else:
+#         for i in range(number,-1,-1):
+#             print(i)
+#         return None
+        
+# Countdown(10)
+
+# def Print_And_Return(list):
+#     if len(list)>2:
+#         print("a list must have 2 values")
+#     else:
+#         print(list[0])
+#         return list[1]
+# listOfNum = [1,2,3]    
+# second = Print_And_Return(listOfNum)
+# print(second)
+
+# def First_Plus_Length(list):
+#     sum = list[0]+len(list)
+#     return sum
+
+# listOfNumbers = [1,2,3,4,5]
+# sum = First_Plus_Length(listOfNumbers)
+# print(sum)
+
+# def Values_Greater_than_Second(list):
+#     if len(list)<2:
+#         return False
+#     else:
+#         value = list[1]
+#         print(value)
+#         for i in list:
+#             if i>value:
+#                 print(i)
+
+# listOfNumbers = [5,2,3,2,1,4]
+# y = Values_Greater_than_Second(listOfNumbers)
+# print(y)
+
+
+def length_and_value(a,b):
+    listOfNumbers =[]
+    for i in range(0,a):
+        listOfNumbers.append(b)
+    return listOfNumbers
+
+ls = length_and_value(4,7)
+print(ls)
