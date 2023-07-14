7370f51a049e3340b1192a3ce4ac7800b14280ad
Amine <mohamedamin.benmakhlouf@gmail.com>
2023-07-14 17:12:57 +0200

    playground

--- 03-flask/Core/playground/server.py
+++ 03-flask/Core/playground/server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/play')
 def index():
-    return templa.html
+    return render_template("index3cercles.html")
+
+@app.route('/play/<int:number>')
+def with_number(number):
+    return render_template("index.html", number=number)
+
+@app.route('/play/<int:number>/<color_name>')
+def color_with_number(number,color_name):
+    return render_template("index.html", number=number, color_name=color_name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5004, debug=True)
