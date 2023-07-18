3cc493acee0e08dede0581c5d2d281eaae07eac0
Amine <mohamedamin.benmakhlouf@gmail.com>
2023-07-18 17:53:54 +0200

    updateDojoSurvey

--- 03-flask/Core/Dojo_Survey/server.py
+++ 03-flask/Core/Dojo_Survey/server.py
@@ -13,12 +13,21 @@ def process():
     session['location']  = request.form['location']
     session['language']  = request.form['language']
     session['comments']  = request.form['comments']
-    print(f"{session['username']} {session['location']} {session['language']} {session['comments']} ")
-    return redirect("result")
+    session['sex']  = request.form['sex']
+    if session['emailupdate'] == "on":
+        session['emailupdate'] = "Yes, you want to receive emails updates"
+    else:
+        session['emailupdate'] = "No, you don't want emails updates"
+    # print(f"{session['username']} {session['location']} {session['language']} {session['comments']} ")
+    return redirect("/result")
 
 @app.route('/result')
 def result():
     return render_template("result.html")
+
+@app.route('/')
+def back():
+    return render_template
 
 if __name__ =='__main__':
     app.run(debug = True, port=5000)
