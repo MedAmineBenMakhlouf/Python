2318f9512fb16a1671aa984ce6d345a1ac893e98
Amine <mohamedamin.benmakhlouf@gmail.com>
2023-07-24 19:27:42 +0200

    userCRUD_Modul

---
+++ 05-flask_mysql/Core/users_crud/flask_app/controllers/user_controller.py
@@ -0,0 +1,58 @@
+from flask import  render_template, redirect, request 
+from flask_app import app
+from flask_app.models.user_model import User
+
+@app.route('/')
+def redirect_to_user():
+    return redirect('/users')
+
+@app.route('/users')
+def dashboard():
+    all_users = User.get_all()
+    return render_template('users.html', users=all_users)
+
+@app.route('/users/new')
+def new_user():
+    return render_template('create.html')
+
+@app.route('/users/create', methods=['POST'])
+def create_user():
+    data_dict = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+    }
+    User.create_user(data_dict)
+    return redirect('/users')
+
+@app.route('/users/view/<int:user_id>')
+def view_user(user_id):
+    data_dict = {'id':user_id}
+    the_user = User.get_by_id(data_dict)
+    return render_template('view.html',user = the_user)
+    
+
+@app.route('/users/<int:user_id>/delete')
+def delete(user_id):
+    data_dict = {'id':user_id}
+    User.delete(data_dict)
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/update')
+def update(user_id):
+    User.update({'id':user_id})
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    user = User.get_by_id({'id':user_id})
+    return render_template('edit.html',user=user)
+
+@app.route('/users/<int:user_id>/update', methods=['POST'])
+def update_user(user_id):
+    data_dict = {**request.form}
+    data_dict['id'] = user_id
+    User.update_user(data_dict)
+    return redirect('/')
+    
+    
---
+++ 05-flask_mysql/Core/users_crud/flask_app/models/user_model.py
@@ -0,0 +1,64 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import DATABASE_NAME 
+class User:
+    def __init__(self, data_dict):
+        self.id = data_dict['id']
+        self.first_name = data_dict['first_name']
+        self.last_name = data_dict['last_name']
+        self.email = data_dict['email']
+        self.created_at = data_dict['created_at']
+        self.updated_at = data_dict['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = """
+        SELECT id,first_name, last_name,email,DATE_FORMAT(created_at, '%M %e, %Y') as created_at,updated_at FROM users;
+        """
+        result = connectToMySQL(DATABASE_NAME).query_db(query)
+        all_users= []
+        for row in result:
+            user = cls(row)
+            all_users.append(user)
+        return all_users
+    
+    @classmethod
+    def get_by_id(cls,data_dict):
+        # query = """SELECT id,first_name, last_name,email, created_at, DATE_FORMAT(updated_at, '%M %e, %Y %h:%i%p') as updated_at 
+        #         FROM users WHERE id=%(id)s"""
+        query = """SELECT * FROM users WHERE id=%(id)s;"""
+        result = connectToMySQL(DATABASE_NAME).query_db(query,data_dict)
+        the_user = cls(result[0])
+        return the_user
+    
+    @classmethod
+    def delete(cls,data_dict):
+        query="DELETE FROM users WHERE id=%(id)s;"
+        result = connectToMySQL(DATABASE_NAME).query_db(query,data_dict)
+        return None
+    
+    # @classmethod
+    # def edit(cls, data_dict):
+    #     query = """SELECT id,first_name, last_name,email,DATE_FORMAT(created_at, '%M %e, %Y') created_at, 
+    #     DATE_FORMAT(updated_at, '%M %e, %Y %h:%i%p') as updated_at FROM users WHERE id=%(id)s"""
+
+        
+    #     result = connectToMySQL("users_schema").query_db(query,data_dict)
+    #     return result
+    
+    @classmethod
+    def update_user(cls,data_dict):
+        query = """
+                UPDATE users set first_name=%(first_name)s , last_name=%(last_name)s, email=%(email)s
+                WHERE id=%(id)s
+                """    
+        
+        return connectToMySQL(DATABASE_NAME).query_db(query,data_dict)
+    
+    @classmethod
+    def create_user(cls,data_dict):
+        query= """
+                INSERT INTO users(first_name,last_name,email)
+                VALUES (%(first_name)s,%(last_name)s,%(email)s);
+                """
+        result = connectToMySQL(DATABASE_NAME).query_db(query,data_dict)
+        return None
